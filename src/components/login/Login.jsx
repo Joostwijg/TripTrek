60ae2f370c6ef133ec26f6faf688ab139477fc13
joostwijg <joost@webwij.nl>
2025-01-09 18:43:56 +0100

    Basis inlog scherm met state

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("email", email);
+        console.log("password", password);
+    };
+
+
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email"> E-mail</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+                <label htmlFor="pass">Password</label>
+                <input
+                    type="password"
+                    id="pass"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+                <button type="submit">Login</button>
+                <button type="submit">Registreren</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
