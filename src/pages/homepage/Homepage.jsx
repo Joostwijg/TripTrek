caecd33ba68d79340fb89a077b01b63ce02c2db3
joostwijg <joost@webwij.nl>
2025-02-26 11:35:41 +0100

    homepagina design 1.0

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,8 +1,45 @@
+import Header from "../../components/header/Header.jsx";
+import './Homepage.css'
+
 const Home = () => {
     return (
-        <div className="homepage">
-            <h1> Welkom op de homepagina</h1>
-            <p>Dit is de plek waar je terechtkomt als je ingelogd bent</p>
+
+        <div className="main-container homepage">
+            <Header />
+            <div className="content-container">
+                <div className="content-left-container">
+                    <div className="content-left-upper-container">
+                        <div className="container-profile-picture">
+                            <img src="/src/assets/test/Mask group.png" alt="" className='profile-picture'/>
+                        </div>
+                        <div className="container-personal-information">
+                            <h2>Dynamic Name</h2>
+                            <p>Registration date</p>
+                            <div className="follow-information">
+                                <div className="follow-information-following">
+                                    <h4>Following</h4>
+                                    <p>350</p>
+                                </div>
+                                <div className="follow-information-followers">
+                                    <h4>Followers</h4>
+                                    <p>350</p>
+                                </div>
+                            </div>
+                            <div className="edit-profile-button">
+                                <p>Edit profile</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="content-right-upper-container">
+
+                    </div>
+                </div>
+                <div className="content-right-container">
+
+                </div>
+
+            </div>
         </div>
     );
 }
